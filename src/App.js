20d1326a94fe027c1bfa2d55a2f5f6333db3e249
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-05-09 23:10:37 +0100

    feat: setup the individual listings page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import Category from './pages/Category'
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           <Route path='/' element={<Explore/>} />
           <Route path='/offers' element={<Offers/>} />
           <Route path='/category/:categoryName' element={<Category/>} />
+          <Route path='/category/:categoryName/:listingId' element={<Listing/>} />
           <Route path='/profile' element={<PrivateRoute/>}>
             <Route path='/profile' element={<Profile/>} />
           </Route>
