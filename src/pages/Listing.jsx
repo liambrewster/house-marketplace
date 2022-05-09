0bd47b80aad1bc3bf4d8287cda76fbafca7b36d8
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-05-09 23:43:17 +0100

    feat: show listing details on the page

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -28,6 +28,10 @@ function Listing() {
         fetchListing()
     },[navigate,params.listingId])
 
+    if(loading) {
+        return <Spinner/>
+    }
+
   return (
     <main>
         {/* Slideshow of images to go here */}
@@ -44,6 +48,29 @@ function Listing() {
     </div>
 
     {shareLinkCopied && <p className='linkCopied'>Link Copied!</p>} 
+
+    <div className="lsitingDetails">
+        <p className="listingName">{listing.name} - £{listing.offer ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',') : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',')}</p>
+        <p className="listingLocation">{listing.location}</p>
+        <p className="listingType">For {listing.type === 'rent' ? 'Rent' : 'Sale'}</p>
+        {listing.offer && (
+            <p className="discountPrice">£{listing.regularPrice - listing.discountedPrice} discount</p>
+        )}
+
+        <ul className="listingDetailsList">
+            <li>{listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}</li>
+            <li>{listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}</li>
+            <li>{listing.parking && 'Parking Spot'}</li>
+            <li>{listing.furnished && 'Furnished'}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+        {/* Map to go here */}
+
+        {auth.currentUser?.uid !== listing.userRef && (
+            <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className='primaryButton'>Contact Landlord</Link>
+        )}
+    </div>
     </main>
   )
 }
